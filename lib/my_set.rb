74625222b16126d76f2eb5961a00ea74f3bbf173
Amanda Smith <ajs.amandasmith@gmail.com>
2022-11-04 16:39:14 -0500

    commit tree 7054cd7d4c05b99eeb55b91e2274ebeb80c710a5

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,51 @@
 # your code here
+class MySet
+
+  def initialize(enumerable = [])
+    @hash = {}
+    enumerable.each do |value|
+      @hash[value] = true
+    end
+  end
+
+  def include?(value)
+    hash.has_key?(value)
+  end
+
+  def add(value)
+    hash[value] = true # add a value as a key on the hash
+    self                # return the updated set
+  end
+
+  def delete(value)
+    hash.delete(value)
+    self
+  end
+
+  def size
+    hash.size
+  end
+
+  def self.[](*args)
+    new(args)
+  end
+
+  def clear
+    hash.clear
+    self
+  end
+
+  def each(&block)
+    self.hash.keys.each(&block)
+    self
+  end
+
+  def inspect
+    "#<#{self.class.name}: {#{self.hash.keys.join(', ')}}>"
+  end
+
+  private
+
+  attr_reader :hash
+
+end
